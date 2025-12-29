92de2a33b396d06a553c172b134239521b5e6822
raindotwav <145295124+raindotwav@users.noreply.github.com>
2025-12-29 18:16:19 -0500

    improved solution by simplifying for loops
    Refactor longestCommonPrefix function for clarity and add docstring.

--- longest_common_prefix/my_solution.py
+++ longest_common_prefix/my_solution.py
@@ -1,27 +1,28 @@
+
 class Solution:
 
-
+    
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        #what this function does:
-        #creates a string 'finalString' which will be printed at the end of the function
-        #pulls the 'count' character from each string in 'strs' and puts them into a new list 'checkList'
-        #assigns a variable checkCharacter to the first character in checkList
-        #if any character in checkList is unequal to checkCharacter, return finalString
-        #if all the characters in checkList are the same, append the first character in checkList to finalString
-        #and increase count by one, so the next character in strs can be examined
+        """this function finds and returns the longest common prefix in a list of strings"""
         goodCharacter = True
         count = 0
+        #creates a string 'finalString' which will be printed at the end of the function
         finalString = ''
         while goodCharacter == True:
             checkList = []
-            for i in range(len(strs)):
-                checkList.append(strs[i][count])
+            #pulls the 'count' character from each string in 'strs' and puts them into a new list 'checkList'
+            for string in strs:
+                checkList.append(string[count])
+            #assigns a variable checkCharacter to the first character in checkList
             checkCharacter = checkList[0]
             
-            for i in range(len(checkList)):
-                if checkList[i] != checkCharacter:
+            for character in checkList:
+                #if any character in checkList is unequal to checkCharacter, return finalString
+                if character != checkCharacter:
                     goodCharacter = False
                     return finalString
+            #if all the characters in checkList are the same, append the first character in checkList to finalString
+            #and increase count by one, so the next character in strs can be examined
             finalString = finalString + checkList[0]
             count += 1
 
